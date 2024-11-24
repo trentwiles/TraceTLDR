437fd01c74eec586278c1065179a0b4f350b05f1
trentwiles <wilest7@gmail.com>
2024-11-23 21:37:36 -0500

    readme.md updates

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Don't have time to search through Trace? Get a quick of any Northeastern profess
 * Second, all comments made on each class are scraped, along with the name of the professor
 * Third, all comments placed under their respective professor
 * Fourth, Google Gemini summarizes the positive comments and the critical comments into five words
+
+# Installation
+
+```shell
+# Download dependencies
+pip3 install -r requirements.txt
+
+# Copy the Template .env
+cp .env.example .env
+
+# Add all required variables to .env
+# cookies - the cookies list for trace, see "How to Get Trace Cookies"
+# mongo - MongoDB connection string
+# gemini- Your Google Gemini API Key
+
+# Scrape the data and upload to mongo, takes a few hours
+screen python3 run.py
+
+# Run the web server in dev mode
+python3 server.py
+```
--- ai.py
+++ ai.py
@@ -2,7 +2,6 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
-import time
 
 load_dotenv()
 
@@ -10,6 +9,6 @@ genai.configure(api_key=os.getenv("gemini"))
 
 def summarize(commentType, comments):
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(f"The following are a few {commentType} comments left by students, separed by commas, given these comments, provide five words describing their preformance, ONLY LIST THE WORDS (separated by commas), nothing more: ${comments}")
+    response = model.generate_content(f"The following are a few {commentType} comments left by students, separed by commas, given these comments, provide five words describing their preformance, ONLY LIST THE LOWERCASE WORDS (separated by commas), nothing more: ${comments}")
     
     return response.text.strip()
